6d21a2b13686e8ccd391088d9033a4d21db6758d
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-05-06 10:48:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "28660719"
 api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7115511008:AAHth-PK1VhLfd3xILZyN_ssqRUQOz0LXnY"
+bot_token = "7115511008:AAHrMT56aLKVcXWsdEslV3jlIRZl2tUU1io"
