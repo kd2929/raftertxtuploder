5a682c23f70942552cf242dd124f7108e6cbde97
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-05-15 22:42:52 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,3 @@
-import os
-
-bot_token=os.environ.get("BOT_TOKEN"),
-api_id=int(os.environ.get("API_ID")),
-api_hash=os.environ.get("API_HASH")
+api_id = "25284113"
+api_hash = "a17e15f84e6cabf51a0cce7fe4e88f7c"
+bot_token = "6452423064:AAHo82mEGI2tHqSUakefeCDR_SyfhbDfWwA"
